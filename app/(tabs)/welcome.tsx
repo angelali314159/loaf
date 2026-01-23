672dbf94bb3957690ad2ba12935c6071907994dd
angelali314159 <angelali314159@gmail.com>
2026-01-23 14:01:21 -0500

    added "hi" to te button

--- app/(tabs)/welcome.tsx
+++ app/(tabs)/welcome.tsx
@@ -1,21 +1,19 @@
-import { router } from 'expo-router';
-import React from 'react';
-import { View } from 'react-native';
-import { Button } from '../../components/typography';
+import { router } from "expo-router";
+import React from "react";
+import { View } from "react-native";
+import { Button } from "../../components/typography";
 
 export default function Welcome() {
-
   return (
-    
-      <View>
-        <Button
-            title="Go to Login"
-            onPress={() => router.push('/(tabs)/login')}
-        />
-        <Button
-            title="Go to Sign Up"
-            onPress={() => router.push('/(tabs)/signUp')}
-        />
-      </View>
+    <View>
+      <Button
+        title="Go to Login hi"
+        onPress={() => router.push("/(tabs)/login")}
+      />
+      <Button
+        title="Go to Sign Up"
+        onPress={() => router.push("/(tabs)/signUp")}
+      />
+    </View>
   );
 }

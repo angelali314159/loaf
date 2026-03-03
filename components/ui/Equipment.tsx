49b45bc57c0657d53fa6bbc28dc405bb9600cbf2
angelali314159 <angelali314159@gmail.com>
2026-03-02 20:19:06 -0500

    updated components to use supabase lists of equipment and muscles

--- components/ui/Equipment.tsx
+++ components/ui/Equipment.tsx
@@ -1,50 +1,62 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Dimensions, Image, Pressable } from 'react-native';
-import { Button, H1, H2, P } from "../typography";
+import { useExerciseLibrary } from "@/contexts/ExerciseLibraryContext";
+import React from "react";
+import { Dimensions, Image, Pressable, View } from "react-native";
+import { P } from "../typography";
 
 interface EquipmentProps {
-    selectedEquipments: string[];
-    setSelectedEquipments: (groups: string[]) => void;
+  selectedEquipments: string[];
+  setSelectedEquipments: (groups: string[]) => void;
 }
 
-export default function Equipment({ selectedEquipments, setSelectedEquipments }: EquipmentProps) {
-    const muscleGroups = [  // Must Change: Placeholder for equipment groups data
-        { name: "Dumbbells", image: require("../../assets/images/dumbbell.png") },
-        { name: "Barbell", image: require("../../assets/images/dumbbell.png") },
-        { name: "Kettlebells", image: require("../../assets/images/dumbbell.png") },
-        { name: "Resistance Bands", image: require("../../assets/images/dumbbell.png") },
-    ]
-    const { width, height } = Dimensions.get("window");
-    var boxWidth = (width - 100) / 3; // 20 padding on each side and 20 gap between boxes
-     if (boxWidth > 150) { // Set a max width for larger screens
-        boxWidth = 150;
-    }
-    const cssNotSelected = `bg-[#f2f0ef] border border-[#B1B0B0] rounded-lg items-center justify-center gap-2`;
-    const cssSelected = `bg-[#FCDE8C] border border-[#FCDE8C] rounded-lg items-center justify-center gap-2`;
-    return (
-        <View className="flex flex-row flex-wrap gap-4">
-        {muscleGroups.map((group, index) => (
-        <Pressable 
-            className={selectedEquipments.includes(group.name) ? cssSelected : cssNotSelected}
-            style={{ width: boxWidth, height: boxWidth, paddingVertical: 10}}
-            key={index}
-            onPress={() => {
-                if (selectedEquipments.includes(group.name)) {
-                    setSelectedEquipments(selectedEquipments.filter(name => name !== group.name));
-                } else {
-                    setSelectedEquipments([...selectedEquipments, group.name]);
-                }
-            }}
+export default function Equipment({
+  selectedEquipments,
+  setSelectedEquipments,
+}: EquipmentProps) {
+  const { equipmentList, loading } = useExerciseLibrary();
+
+  const { width } = Dimensions.get("window");
+  let boxWidth = (width - 100) / 3; // 20 padding on each side and 20 gap between boxes
+  if (boxWidth > 150) {
+    // Set a max width for larger screens
+    boxWidth = 150;
+  }
+
+  const cssNotSelected = `bg-[#f2f0ef] border border-[#B1B0B0] rounded-lg items-center justify-center gap-2`;
+  const cssSelected = `bg-[#FCDE8C] border border-[#FCDE8C] rounded-lg items-center justify-center gap-2`;
+
+  if (loading) {
+    return <P>Loading equipment...</P>;
+  }
+
+  return (
+    <View className="flex flex-row flex-wrap gap-4">
+      {equipmentList.map((equipment, index) => (
+        <Pressable
+          className={
+            selectedEquipments.includes(equipment)
+              ? cssSelected
+              : cssNotSelected
+          }
+          style={{ width: boxWidth, height: boxWidth, paddingVertical: 10 }}
+          key={index}
+          onPress={() => {
+            if (selectedEquipments.includes(equipment)) {
+              setSelectedEquipments(
+                selectedEquipments.filter((name) => name !== equipment),
+              );
+            } else {
+              setSelectedEquipments([...selectedEquipments, equipment]);
+            }
+          }}
         >
-            <Image
-                className=""
-                source={group.image}
-                resizeMode="contain"
-            />
-            <P className={`w-[${boxWidth}] h-[${boxWidth}] flex-wrap text-center`}>{group.name}</P>
+          <Image className="" source={equipment.image} resizeMode="contain" />
+          <P
+            className={`w-[${boxWidth}] h-[${boxWidth}] flex-wrap text-center`}
+          >
+            {equipment}
+          </P>
         </Pressable>
-        ))
-        }
-        </View>
-    )
-};
+      ))}
+    </View>
+  );
+}
